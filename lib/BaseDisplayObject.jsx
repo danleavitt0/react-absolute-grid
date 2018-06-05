d16c88699bc1ed1523c942e981f39a3b4c9586ce
danleavitt0 <danleavitt0@gmail.com>
2018-06-04 20:55:22 -0700

    update values

--- lib/BaseDisplayObject.jsx
+++ lib/BaseDisplayObject.jsx
@@ -1,13 +1,16 @@
-'use strict';
+'use strict'
 
-import React, { Component, PureComponent } from 'react';
+import React, { Component, PureComponent } from 'react'
 
-import LayoutManager from './LayoutManager.js';
-import PropTypes from 'prop-types';
+import LayoutManager from './LayoutManager.js'
+import PropTypes from 'prop-types'
 
-export default function createDisplayObject(DisplayObject, displayProps, forceImpure) {
-
-  const Comp = forceImpure ? Component : PureComponent;
+export default function createDisplayObject(
+  DisplayObject,
+  displayProps,
+  forceImpure
+) {
+  const Comp = forceImpure ? Component : PureComponent
 
   return class extends Comp {
     static propTypes = {
@@ -23,23 +26,28 @@ export default function createDisplayObject(DisplayObject, displayProps, forceIm
 
     updateDrag(x, y) {
       //Pause Animation lets our item return to a snapped position without being animated
-      let pauseAnimation = false;
-      if(!this.props.dragManager.dragItem){
-        pauseAnimation = true;
+      let pauseAnimation = false
+      if (!this.props.dragManager.dragItem) {
+        pauseAnimation = true
         setTimeout(() => {
-          this.setState({pauseAnimation: false});
-        }, 20);
+          this.setState({ pauseAnimation: false })
+        }, 20)
       }
       this.setState({
         dragX: x,
         dragY: y,
         pauseAnimation: pauseAnimation
-      });
+      })
     }
 
-    onDrag = (e) => {
-      if(this.props.dragManager){
-        this.props.dragManager.startDrag(e, this.domNode, this.props.item, this.updateDrag.bind(this));
+    onDrag = e => {
+      if (this.props.dragManager) {
+        this.props.dragManager.startDrag(
+          e,
+          this.domNode,
+          this.props.item,
+          this.updateDrag.bind(this)
+        )
       }
     }
 
@@ -49,54 +57,83 @@ export default function createDisplayObject(DisplayObject, displayProps, forceIm
         itemHeight: this.props.itemHeight,
         verticalMargin: this.props.verticalMargin,
         zoom: this.props.zoom
-      };
-      const layout = new LayoutManager(options, this.props.layoutWidth);
-      const style = layout.getStyle(this.props.index,
-        this.props.animation,
-        this.props.item[this.props.filterProp]);
-      //If this is the object being dragged, return a different style
-      if (this.props.dragManager.dragItem &&
-          this.props.dragManager.dragItem[this.props.keyProp] === this.props.item[this.props.keyProp]) {
-        const dragStyle = this.props.dragManager.getStyle(this.state.dragX, this.state.dragY);
-        return {...style, ...dragStyle};
-      } else if (this.state && this.state.pauseAnimation) {
-        const pauseAnimationStyle = {...style};
-        pauseAnimationStyle.WebkitTransition = 'none';
-        pauseAnimationStyle.MozTransition = 'none';
-        pauseAnimationStyle.msTransition = 'none';
-        pauseAnimationStyle.transition = 'none';
-        return pauseAnimationStyle;
       }
-      return style;
+      const layout = new LayoutManager(options, this.props.layoutWidth)
+      const style = layout.getStyle(
+        this.props.index,
+        this.props.animation,
+        this.props.item[this.props.filterProp]
+      )
+      //If this is the object being dragged, return a different style
+      if (
+        this.props.dragManager.dragItem &&
+        this.props.dragManager.dragItem[this.props.keyProp] ===
+          this.props.item[this.props.keyProp]
+      ) {
+        const dragStyle = this.props.dragManager.getStyle(
+          this.state.dragX,
+          this.state.dragY
+        )
+        return { ...style, ...dragStyle }
+      } else if (this.state && this.state.pauseAnimation) {
+        const pauseAnimationStyle = { ...style }
+        pauseAnimationStyle.WebkitTransition = 'none'
+        pauseAnimationStyle.MozTransition = 'none'
+        pauseAnimationStyle.msTransition = 'none'
+        pauseAnimationStyle.transition = 'none'
+        return pauseAnimationStyle
+      }
+      return style
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!this.props.dragEnabled && nextProps.dragEnabled) {
+        console.log('add drag')
+        this.domNode.addEventListener('mousedown', this.onDrag)
+        this.domNode.addEventListener('touchstart', this.onDrag)
+        this.domNode.setAttribute(
+          'data-key',
+          this.props.item[this.props.keyProp]
+        )
+      }
+      if (this.props.dragEnabled && !nextProps.dragEnabled) {
+        console.log('remove drag')
+        this.props.dragManager.endDrag()
+        this.domNode.removeEventListener('mousedown', this.onDrag)
+        this.domNode.removeEventListener('touchstart', this.onDrag)
+      }
     }
 
     componentDidMount() {
       if (this.props.dragEnabled) {
-        this.domNode.addEventListener('mousedown', this.onDrag);
-        this.domNode.addEventListener('touchstart', this.onDrag);
-        this.domNode.setAttribute('data-key', this.props.item[this.props.keyProp]);
+        this.domNode.addEventListener('mousedown', this.onDrag)
+        this.domNode.addEventListener('touchstart', this.onDrag)
+        this.domNode.setAttribute(
+          'data-key',
+          this.props.item[this.props.keyProp]
+        )
       }
     }
 
     componentWillUnmount() {
       if (this.props.dragEnabled) {
-        this.props.dragManager.endDrag();
-        this.domNode.removeEventListener('mousedown', this.onDrag);
-        this.domNode.removeEventListener('touchstart', this.onDrag);
+        this.props.dragManager.endDrag()
+        this.domNode.removeEventListener('mousedown', this.onDrag)
+        this.domNode.removeEventListener('touchstart', this.onDrag)
       }
     }
 
     render() {
       return (
-        <div ref={node => this.domNode = node}
-             style={this.getStyle()}>
+        <div ref={node => (this.domNode = node)} style={this.getStyle()}>
           <DisplayObject
             {...displayProps}
             item={this.props.item}
             index={this.props.index}
-            itemsLength={this.props.itemsLength}/>
+            itemsLength={this.props.itemsLength}
+          />
         </div>
-      );
+      )
     }
   }
 }
